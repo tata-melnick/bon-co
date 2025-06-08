6d7b652a819513765e5ef87e0a435b74f7708a3f
Tatiana Melnick <tata.slobyanyuk@mail.ru>
2025-06-09 01:13:33 +0300

    Add component footer [Footer] (Layout)

---
+++ src/layout/footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+import footerLogo from "../../assets/bon-co-footer-logo.svg";
+import footerSupport from "../../assets/footer-support.png";
+import instagram from "../../assets/instagram.svg";
+import telegram from "../../assets/telegram.svg";
+import whatsApp from "../../assets/whats-app.svg";
+import { ExtLink, Link } from "../../common";
+import { routes } from "../../constants";
+
+import styles from "./Footer.module.css";
+
+export const Footer = () => {
+    return (
+        <>
+            <div className={styles.footer}>
+                <nav className={styles.nav}>
+                    <Link to={routes.company}>О компании /</Link>
+                    <Link to={routes.gallery}>Галерея товаров /</Link>
+                    <Link to={routes.reviews}>Отзывы</Link>
+                </nav>
+
+                <Link to={routes.main}>
+                    <img src={footerLogo} alt="Логотип компании BON & CO" />
+                </Link>
+                <div className={styles.socials}>
+                    <ExtLink>
+                        <img src={whatsApp} alt="WhatsApp" />
+                    </ExtLink>
+                    <ExtLink>
+                        <img src={telegram} alt="Telegram" />
+                    </ExtLink>
+                    <ExtLink>
+                        <img src={instagram} alt="Instagram" />
+                    </ExtLink>
+                </div>
+            </div>
+            <div className={styles.support}>
+                <p>2024 © Бондарев Евгений Владимирович Все права защищены. Политика конфиденциальности</p>
+                <img className={styles.supportImg} src={footerSupport} alt="Разработанно при поддержке мой бизнес" />
+            </div>
+        </>
+    );
+};
